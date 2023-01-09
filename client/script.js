eec1819779fd652e3b602443270ac1abf8f01f54
BrownYuri <102958787+AvacadoDEV@users.noreply.github.com>
2023-01-09 17:56:56 -0400

    changes in package

--- client/script.js
+++ client/script.js
@@ -51,7 +51,7 @@ function chatStripe(isAi, value, uniqueId) {
             <div class="chat">
                 <div class="profile">
                     <img 
-                      src="${isAi ? bot : user}" 
+                      src=${isAi ? bot : user} 
                       alt="${isAi ? 'bot' : 'user'}" 
                     />
                 </div>
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('http://localhost:5000/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
